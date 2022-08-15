ff86f41ec9a59329098e32b8643f54c4f525cc78
Ahmed shalaby <97378406+shalaby2022@users.noreply.github.com>
2022-08-15 02:03:42 +0200

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, ScrollView, FlatList, TextInput, StyleSheet, RefreshControl,Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Card from '../common/Card';
+import Footer from '../common/Footer';
+
+
+const Home = ({ navigation }) => {
+
+    const [news, setNews] = useState([])
+    const [filteredNews, setFilteredNews] = useState([])
+    const [search, setSearch] = useState('')
+    const [refresh, setRefresh] = useState(false)
+
+    useEffect(()=> {
+        fetchNews()
+    },[])
+
+    const searchFilter = (input)=> {
+        if (input) {
+            const newData = news.filter((item)=> {
+                const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+                const inputData = input.toUpperCase();
+                return itemData.indexOf(inputData) > -1;
+            })
+            setFilteredNews(newData);
+            setSearch(input)
+        } else {
+            setFilteredNews(news)
+            setSearch(input)
+        }
+    }
+
+    const fetchNews = ()=> {
+        return fetch('https://newsapi.org/v2/everything?q=tesla&from=2022-07-14&sortBy=publishedAt&apiKey=5511b6e00c9f46e69392688d68a92bbf')
+                .then(res => res.json())
+                .then(({articles}) => setNews(articles))
+                .then(({articles}) => setFilteredNews(articles))
+                .catch(err => console.log(err))
+    }
+
+    const ItemView = ({item}) => {
+        return (
+            <View>
+            <Card 
+                img={item.urlToImage} 
+                title={item.title} 
+                navigation={navigation} 
+                description={item.description}
+                author={item.author}
+                publishedAt={item.publishedAt}
+            />
+        </View>
+        )
+    }
+
+    const ItemSeparatorView = ()=> {
+        return( 
+            <View
+            style={{height: 0.5,width: '100%', backgroundColor: '#c8c8c8'}}
+            />
+        )
+    }
+
+    const pull = ()=> {
+        setRefresh(true)
+        fetchNews()
+        setTimeout(()=> {
+            setRefresh(false)
+        }, 1500)
+    }
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#ccc' }}>
+            <View style={{ flex: 1}}>
+                <View style={styles.views}>
+                    <Image
+                    style={{ width: 35, height: 35 }}
+                    source={require('../images/newspaper.png')}
+                    />
+                    <TextInput
+                    style={styles.input}
+                    value={search}
+                    placeholder='Search'
+                    underlineColorAndroid="transparent"
+                    onChangeText={(input)=> {
+                        searchFilter(input)
+                    }}
+                />
+                </View>
+                <FlatList
+                    style={{ padding: 6, flex: 1}}
+                    data={filteredNews}
+                    keyExtractor={(item,index)=>index.toString()}
+                    ItemSeparatorComponent={ItemSeparatorView}
+                    renderItem={ItemView}
+                    refreshControl={
+                            <RefreshControl
+                                refreshing={refresh}
+                                onRefresh={()=> pull()}
+                                colors={['green','orange','red']}
+                            />
+                        }
+                />
+            </View>
+            <Footer navigation={navigation} />
+        </SafeAreaView>
+        
+    )
+}
+
+export default Home
+
+
+const styles = StyleSheet.create({
+    views: {
+        flexShrink: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "gray",
+        padding: 10,
+    }, 
+    itemStyle: {
+        padding: 15,
+    },
+    input: {
+        height: 40,
+        width: 200,
+        borderWidth: 1,
+        paddingLeft: 5,
+        margin: 5,
+        borderColor: 'black',
+        borderRadius: 5,
+        backgroundColor: 'white'
+    }
+})
